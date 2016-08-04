3b8f675315229ef9ff6fa4ceb2b5cc535ffa0ccb
Justin Bull <bull.justin@gmail.com>
2016-08-03 22:02:54 -0700

    Fix webpack dev server config

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,60 @@
-const path = require("path");
+const path = require('path');
 const autoprefixer = require('autoprefixer');
 const precss = require('precss');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const LiveReloadPlugin = require('webpack-livereload-plugin');
 
+const METADATA = {
+  title: 'Webpack Demo',
+  baseUrl: '/',
+  host: 'localhost',
+  port: 3000
+};
+
+const nodeModulesPath = path.resolve(__dirname, 'node_modules');
+const srcPath = path.resolve(__dirname, 'src');
+const distPath = path.resolve(__dirname, 'dist');
+
 module.exports = {
-    context: path.join(__dirname, "src"),
-    entry: "assets/js/app.js",
+    metadata: METADATA,
+    entry: [
+      path.resolve(srcPath, 'assets', 'js', 'app.js')
+    ],
     output: {
-        path: path.join(__dirname, "dist"),
-        filename: 'assets/js/[name].js'
+        path: path.resolve(distPath, 'assets'),
+        publicPath: '/assets/',
+        filename: 'js/bundle.js'
     },
     module: {
         loaders: [
-          { test: /\.js$/, exclude: /(node_modules)/, loader: 'babel', query: { presets: ['es2015'] }},
-          { test: /\.css$/, loader: "style!css!postcss" },
-          { test: /\.scss$/, loader: ExtractTextPlugin.extract("style", "css!postcss!sass") }
+          { test: /\.js$/, loader: 'babel', exclude: [nodeModulesPath], query: { presets: ['es2015'] }},
+          { test: /\.css$/, loader: 'style!css!postcss' },
+          { test: /\.scss$/, loader: ExtractTextPlugin.extract('style', 'css!postcss!sass') }
         ]
     },
     plugins: [
-        new ExtractTextPlugin('assets/css/[name].css'),
-        new HtmlWebpackPlugin(),
+        new ExtractTextPlugin('css/bundle.css'),
+        new HtmlWebpackPlugin({
+          template: path.resolve(srcPath, 'index.html'),
+          filename: path.resolve(distPath, 'index.html')
+        }),
         new LiveReloadPlugin()
     ],
     resolve: {
         extensions: ['', '.js', '.scss'],
-        root: [path.join(__dirname, 'src')]
+        root: [srcPath]
     },
     postcss: function () {
         return [precss, autoprefixer];
+    },
+    devServer: {
+      port: METADATA.port,
+      host: METADATA.host,
+      watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000
+      },
+      contentBase: distPath
     }
 };
